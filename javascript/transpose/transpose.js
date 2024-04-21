f41525c057cc465676d3c47e7ea8c31b5ddfdc99
Smabian <5991984+Smabian@users.noreply.github.com>
2024-04-21 10:25:13 +0200

    Add improved version to transpose exercise

--- javascript/transpose/transpose.js
+++ javascript/transpose/transpose.js
@@ -1,12 +1,6 @@
-//
-// This is only a SKELETON file for the 'Transpose' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export const transpose = (inputArr) => {
-  if (inputArr.length < 1) {return []};
 
-  let maxSize = Math.max(...(inputArr.map(el => el.length)));
+  let maxSize = Math.max(0,...(inputArr.map(el => el.length)));
   let outputArr = new Array(maxSize).fill("");  
 
   //Transposing
@@ -15,23 +9,13 @@ export const transpose = (inputArr) => {
     let temp = inputArr[row].replace(/ /g, "_").split('');
     
     for (let column = 0; column < maxSize; column++) {
-      outputArr[column] = outputArr[column] + temp[column];
-      outputArr[column] = outputArr[column].replace(/undefined/g, " ")
+      outputArr[column] = outputArr[column] + (temp[column] ?? " ");
     }
   }
-
-  // Trimming
+  //Trimming
   for (let i = outputArr.length-1; i >= 0; i--) {
-    if(outputArr[i].slice(-1) != " ") {
-        
-      //*** Replacing Whitespace placeholder / Alternative Solution would be helpful ***
-        for (let j = 0; j < outputArr.length; j++){
-          outputArr[j] = outputArr[j].replace(/_/g, " ");
-        }
-
-      return outputArr;
-    } else {
-      outputArr[i] = outputArr[i].trimEnd();
-    }
-  }
+    outputArr[i] = outputArr[i].trimEnd();}
+  for (let j = 0; j < outputArr.length; j++){
+    outputArr[j] = outputArr[j].replace(/_/g, " ");}
+  return outputArr;
 };
